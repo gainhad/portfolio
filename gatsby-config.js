9b984331a9b0346de096d70b49a5c5c3b1985ae5
Hadley Gaines <hgaines@umich.edu>
2019-07-18 13:31:07 -0400

    Built out landing and contact pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Hadley Gaines' Portfolio`,
+    description: `Professional Portfolio for Hadley Gaines`,
+    author: `Hadley Gaines`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -13,22 +13,16 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-typography',
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        pathToConfigModule: `src/utils/typography`,
       },
     },
+    `gatsby-plugin-sass`,
+    `gatsby-transformer-sharp`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ],
-}
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,30 @@
-import React from "react"
-import { Link } from "gatsby"
-
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import React from 'react';
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+import styles from './index.module.scss';
+import ListLink from '../components/ListLink';
+import NavList from '../components/NavList';
 
 const IndexPage = () => (
-  <Layout>
+  <>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div className={styles.index}>
+      <h1 className={styles.name}>Hadley Gaines</h1>
+      <p className={styles.tagLine}>
+        Here will be a descriptive line that I haven't thought of yet
+      </p>
+      <img
+        src="https://assets.teenvogue.com/photos/5b1dca8039ea107bc58a20e5/1:1/w_662,h_662,c_limit/tout.jpg"
+        alt="Headshot of Hadley Gaines"
+        className={styles.profilePhoto}
+      ></img>
+      <NavList id={styles.navList}>
+        <ListLink to="/about">About Me</ListLink>
+        <ListLink to="/projects">My Projects</ListLink>
+        <ListLink to="/contact">Contact Me</ListLink>
+      </NavList>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+  </>
+);
 
-export default IndexPage
+export default IndexPage;
