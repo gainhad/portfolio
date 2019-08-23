d6e8a270b2fe4c37cbafd8fcb7d2ba91cea8d36d
gainhad <hadley.gaines@gmail.com>
2019-08-22 22:31:12 -0400

    Update list link component

--- src/components/ListLink.js
+++ src/components/ListLink.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { Link } from 'gatsby';
+
+
+
+
 import styles from './ListLink.module.scss';
 
 const ListLink = props => (
   <li>
-    <Link className={styles.listLink} to={props.to}>
+    <Link
+      className={props.className}
+      to={props.to}
+      activeClassName={props.activeClassName}
+    >
       {props.children}
     </Link>
   </li>
