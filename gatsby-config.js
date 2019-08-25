2f62b626ff5919be787e9fba95a67856f17b89bf
Hadley Gaines <hadley.gaines@gmail.com>
2019-08-25 16:54:55 -0400

    Added favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,18 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Hadley Gaines' Portfolio`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: `src/images/icon.png`,
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
